ac16b96d0d16942eec546195c1441776689628cc
DaisyGeraldine <chipanal@hotmail.com>
2022-06-07 18:38:27 -0700

    Print last digit of a number random

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -19,11 +19,11 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, d);
 	}
-	if ( d==0 )
+	if (d == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, d);
 	}
-	if ( d < 6 && d >=1 )
+	if (d < 6 && d >=1)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, d);
 	}
