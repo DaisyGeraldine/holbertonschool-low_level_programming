9bb1d209380b1674fd8fb7c706c05420a74b1740
DaisyGeraldine <chipanal@hotmail.com>
2022-06-07 20:00:09 -0700

    Show the last digit of the number

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -10,24 +10,25 @@
  */
 int main(void)
 {
-	int n;
-	int d;
-	srand(time(0));
+	int n, d;
 
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	d = n % 10;
-
 	if (d > 5 && d <= 9)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, d);
 	}
-	if (d == 0)
+	if (d < 6)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, d);
-	}
-	if (d < 6 && d >= 1)
-	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, d);
+		if (d == 0)
+		{
+			printf("Last digit of %d is %d and is 0\n", n, d);
+		}
+		else
+		{
+			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, d);
+		}
 	}
 	return (0);
 }
