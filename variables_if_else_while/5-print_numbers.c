8d9e6056c6e251af1170f1138c14458fa5500624
DaisyGeraldine <chipanal@hotmail.com>
2022-06-08 04:55:21 -0700

    Shows single digit numbers to 10

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{char ch = '0';
+	while (ch <= '9')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
