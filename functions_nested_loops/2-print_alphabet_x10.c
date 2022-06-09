83ff5b5891f15b1f0d18594c4ea5199a0f5a4504
DaisyGeraldine <chipanal@hotmail.com>
2022-06-09 14:56:32 -0700

    Shows 10 times the alphabet, in lowercase

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - 'Prints the alphabet'
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char c = 97;
+
+	while (i <= 10)
+	{
+		c = 97;
+		while (c <= 122)
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
