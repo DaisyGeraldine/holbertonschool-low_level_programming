7c41df76e6e49899d5d25d49c27bf4d5de86acf7
DaisyGeraldine <chipanal@hotmail.com>
2022-06-10 10:41:05 -0700

    Adds the sum

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - task 09
+ * @a : numero 1
+ * @b : numero 2
+ * Description: unction that adds two integers and returns the result
+ * Return: always
+ */
+int add(int a, int b)
+{
+	int suma;
+
+	suma = a + b;
+	return (suma);
+}
