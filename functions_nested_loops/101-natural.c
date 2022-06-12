1e24a28c3d200f7b8b75bd8ccf6e59d7b7d48719
DaisyGeraldine <chipanal@hotmail.com>
2022-06-12 12:55:02 -0700

    Sum of numbers that are multiples of 3 o 5

---
+++ functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - task 12
+ * Description: Computes the sum of the multiples of 3 or 5
+ * Return: always
+ */
+int main(void)
+{
+	int n = 0, sum;
+	for (n = 0; n < 1024; n++)
+	{
+		if((n % 3) == 0 || (n % 5) == 0)
+		{
+			sum += n;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
