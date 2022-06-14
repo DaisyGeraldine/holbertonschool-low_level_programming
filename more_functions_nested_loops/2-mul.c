71b6b2bcb580cb7cff4bbe860fbfc1985d8e3329
DaisyGeraldine <chipanal@hotmail.com>
2022-06-14 08:26:22 -0700

    Multiplies two integers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - task 01
+ * @a : variable 1th to multiplicate
+ * @b : variable 2th to multiplicate
+ * Description:  function that checks for a digit (0 through 9)
+ * Return: always
+ */
+int mul(int a, int b)
+{
+	int mult = 0;
+
+	mult = a * b;
+	return (mult);
+}
