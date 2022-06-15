3f842e19377d58b2628b8c0d294b4b1447eec16e
DaisyGeraldine <chipanal@hotmail.com>
2022-06-15 05:03:17 -0700

    Takes a pointer

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - task 00
+ * @*n : value of a pointer
+ * Description: function that takes a pointer
+ * Return: always
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
