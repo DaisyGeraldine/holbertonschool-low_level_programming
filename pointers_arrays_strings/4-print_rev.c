b542e3d3212eb61393b239b9f4ee774d577bf634
root <root@DAISY-LAPTOP-5GFGKLA5.localdomain>
2022-06-16 09:29:37 -0500

    Prints a string, in reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - task 04
+ * @s : Variable char to validate
+ * Description: function that prints a string, in reverse
+ * Return: always
+ */
+void print_rev(char *s)
+{
+	int i = 0, j;
+
+	while (s[i] != '\0')
+		i++;
+
+	for (j = i ; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar(10);
+}
