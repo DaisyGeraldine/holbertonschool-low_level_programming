7d31cc2816fcd02f5097c9bb006bf30c51628ccb
root <root@DAISY-LAPTOP-5GFGKLA5.localdomain>
2022-06-16 15:17:49 -0500

    Prints half of a string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts_half - task 07
+ * @str : Variable char to validate
+ * Description:  function that prints half of a string, followed by a new line
+ * Return: always
+ */
+void puts_half(char *str)
+{
+	int i = 0, j;
+
+	while (str[i] != '\0')
+		i++;
+
+	i = (i / 2);
+
+	for (j = i; str[j] != '\0'; j++)
+	{
+		_putchar(str[j]);
+	}
+	_putchar(10);
+}
