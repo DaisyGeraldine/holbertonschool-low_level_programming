b334efaacc2f62edd78210e73e1438506c331ecb
root <root@DAISY-LAPTOP-5GFGKLA5.localdomain>
2022-06-18 03:17:10 -0500

    Add concatenation two strings

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _strcat - task 01
+ * @dest : Cadena "Hello"
+ * @src : Cadena "World\n"
+ * Description: function that concatenates two strings
+ * Return: always
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j = 0, k, f = 0;
+
+	while (*(dest + i))
+	{
+		i++;
+	}
+
+	while (*(src + j))
+	{
+		j++;
+	}
+
+	j--;
+
+	for (k = i; k < (i + j); k++)
+	{
+		dest[k] = src[f];
+		f++;
+	}
+	dest[k] = '\n';
+	return (dest);
+}
