7ef1269d9f355db888d5bb020c731431a1b713f1
root <root@DAISY-LAPTOP-5GFGKLA5.localdomain>
2022-06-19 17:23:36 -0500

    Capitalize the first word

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * cap_string - task 06
+ * @s : Array
+ * Description: function that capitalizes all words of a string
+ * Return: always
+ */
+char *cap_string(char *s)
+{
+	int i = 0, len = 0;
+
+	while (*(s + len))
+		len++;
+
+	for (; i < len; i++)
+		if (i > 0)
+		{
+			if (s[i - 1] == ' ' || s[i - 1] == '\n' || s[i - 1] == '\t')
+			{
+				if (s[i] >= 'a' && s[i] <= 'z')
+					s[i] = s[i] - 32;
+			}
+		}
+	return (s);
+}
