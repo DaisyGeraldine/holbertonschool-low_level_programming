8e950983db483357cf871dc1e029d7e74b1f5be4
root <root@DAISY-LAPTOP-5GFGKLA5.localdomain>
2022-06-22 21:18:31 -0500

    multiplies two numbers

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - task 03
+ * @argc : "argument count"
+ * @argv : "argument vector"
+ * Description: Write a program that multiplies two numbers.
+ * Return: always
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	int mul = 0;
+
+	if (argc > 1)
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+	return (0);
+}
