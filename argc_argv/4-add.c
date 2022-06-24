b3d77e142e395dee3ec548b30140ecbf19b4333c
root <root@DAISY-LAPTOP-5GFGKLA5.localdomain>
2022-06-23 20:36:21 -0500

    adds positive numbers

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <errno.h>
+#include <limits.h>
 #include "main.h"
 /**
  * main - task 04
@@ -10,25 +13,73 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, sum;
+	int i, sum, csum = 0;
 
 	if (argc == 1)
 	{
 		sum = 0;
 	}
 
-	for (i = 1; i < argc; i++)
+	for (i = 1; argv[i] != NULL; i++)
 	{
-		if (atoi(argv[i]) > 0)
+		if (int_validation(argv[i]) == 1)
 		{
 			sum += atoi(argv[i]);
-		}
-		else
-		{
-			printf("Error\n");
-			return (1);
+			csum++;
 		}
 	}
-	printf("%d\n", sum);
+	if (csum == (argc - 1))
+		printf("%d\n", sum);
+	else
+		printf("Error\n");
 	return (0);
+}
+
+/**
+ * int_validation - task 04
+ * @s : "argument char"
+ * Description: Write a program that adds positive numbers.
+ * Return: always
+ */
+int int_validation(const char *s)
+{
+	/* Convert*/
+	int val, base = 0, resint;  /* Use 10 for base 10 only input*/
+	char *endptr;
+
+/* If leading space not OK*/
+/* isspace() only valid in unsigned char range and EOF.*/
+	if (isspace((unsigned char) *s))
+	{
+		resint = 5;
+		return (resint); /*Fail - leading spaces*/
+	}
+	errno = 0;
+	val = strtol(s, &endptr, base);
+
+	if (s == endptr)
+	{ /* When endptr is same as s, no conversion happened.*/
+		resint = 5;
+		return (resint);/*"Fail - no conversion"*/
+	}
+/* detect overflow*/
+	if (errno == ERANGE || val < INT_MIN || val > INT_MAX)
+	{
+		resint = 5;
+		return (resint);/*"Fail - overflow"*/
+	}
+/* If trailing space OK, seek pass them*/
+	while (isspace((unsigned char) *endptr))
+	{
+		endptr++;
+	}
+/* If trailing non-numeric text bad*/
+	if (*endptr)
+	{
+		resint = 5;
+		return (resint);/*"Fail - overflow"*/
+	}
+	/*printf("Success %d\n", (int) val);*/
+	resint = 1;
+	return (resint);
 }
--- argc_argv/main.h
+++ argc_argv/main.h
@@ -1,4 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
 int _putchar(char c);
+int int_validation(const char *s);
 #endif
