8d77f94ab77a01d4a73e8d29110b7849f68444f0
root <root@DAISY-LAPTOP-5GFGKLA5.localdomain>
2022-06-26 17:27:02 -0500

    contains a copy of the string

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strdup - task 01
+ * @str : size of the array
+ * Description: Function that returns a pointer to a newly allocated
+ * space in memory.
+ * Return: always
+ */
+char *_strdup(char *str)
+{
+	int i = 0, len = 0;
+	char *cad;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	len++;
+
+	cad = malloc(len * sizeof(char));
+
+	for (i = 0; i < len; i++)
+	{
+		cad[i] = str[i];
+	}
+	return (cad);
+}
