41034a724f4d8f28572d27825a4dc0d5ec7e9ea9
root <root@DAISY-LAPTOP-5GFGKLA5.localdomain>
2022-06-26 23:52:40 -0500

    concatenates two strings

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -12,36 +12,37 @@ char *str_concat(char *s1, char *s2)
 	char *str;
 	int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
 	{
-		str = "";
+		s1 = "";
 	}
-	else
+	if (s2 == NULL)
 	{
-		while (s1[len1] != '\0')
-		{
-			len1++;
-		}
-		while (s2[len2] != '\0')
-		{
-			len2++;
-		}
+		s2 = "";
+	}
+	while (s1[len1] != '\0')
+	{
+		len1++;
+	}
+	while (s2[len2] != '\0')
+	{
 		len2++;
-		str = malloc(len1 + len2 * sizeof(char));
+	}
+	len2++;
+	str = malloc(len1 + len2 * sizeof(char));
 
-		if (str == NULL)
-		{
-			return (NULL);
-		}
-		for (i = 0; i < len1; i++)
-		{
-			str[i] = s1[i];
-		}
-		for (i = len1; i < (len1 + len2); i++)
-		{
-			str[i] = s2[j];
-			j++;
-		}
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < len1; i++)
+	{
+		str[i] = s1[i];
+	}
+	for (i = len1; i < (len1 + len2); i++)
+	{
+		str[i] = s2[j];
+		j++;
 	}
 	return (str);
 }
