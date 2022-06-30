356b4d25e51e0e8346741cbc364ab532cb6d05c4
root <root@DAISY-LAPTOP-5GFGKLA5.localdomain>
2022-06-29 20:14:27 -0500

    Add function calloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -9,7 +9,8 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ptr;
+	unsigned int i = 0;
+	char *ptr;
 
 	ptr = malloc(nmemb * size);
 
@@ -18,6 +19,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
+
+	for (i = 0; i < (nmemb * size); i++)
+		ptr[i] = 0;
 
 	return (ptr);
 }
