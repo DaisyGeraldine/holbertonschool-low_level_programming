4c21c3a19991693d93953a7bb33ceb314af312f1
root <root@DAISY-LAPTOP-5GFGKLA5.localdomain>
2022-07-01 10:39:06 -0500

    Define a new type struct dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H
+#define _DOG_H
+#include <stdio.h>
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* #ifndef _DOG_H */
