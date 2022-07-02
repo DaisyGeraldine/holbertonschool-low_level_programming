4d1b67f6d6be00e2a69425e30e61e138918f4f50
root <root@DAISY-LAPTOP-5GFGKLA5.localdomain>
2022-07-02 18:27:49 -0500

    frees dogs

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - task 05
+ * @d : pointer dog
+ * Description: function that frees dogs
+ * Return: always
+ */
+void free_dog(dog_t *d)
+{
+	free(d);
+	/*free(d->name);*/
+	/*free(d->owner);*/
+}
