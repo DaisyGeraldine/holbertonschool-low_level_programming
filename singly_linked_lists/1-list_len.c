b1576b4a79882b1e3ed8a48fb29be75ecf1048bf
DaisyGeraldine <chipanal@hotmail.com>
2022-07-14 19:41:51 -0500

    List length

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * print_len - task 01
+ * @h : pointer of struct list_t
+ * Description: function that returns the number of elements in a
+ * linked list_t list.
+ * Return: number of items
+ */
+size_t list_len(const list_t *h)
+{
+	size_t c = 0;
+
+	while (h)
+	{
+		h = h->next;
+		c++;
+	}
+
+	return (c);
+}
