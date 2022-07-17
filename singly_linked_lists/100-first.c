e13b06fb0bfab45e2489db01db109c2ec7049fe6
DaisyGeraldine <chipanal@hotmail.com>
2022-07-17 16:21:21 -0500

    prints  before the main function

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * myStartupFun - task 05
+ * Description: function that prints before the main function is executed.
+ * Return: always
+ */
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore myhouse upon my back!\n");
+}
