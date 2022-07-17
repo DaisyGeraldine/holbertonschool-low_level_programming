fd59bfc5790ff79db0b90f641e54fd3291e92ab7
DaisyGeraldine <chipanal@hotmail.com>
2022-07-17 16:25:30 -0500

    Prints before the main function

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -7,6 +7,6 @@
  */
 void myStartupFun(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
+	printf("You're beat! and yet, you must allow, \n");
 	printf("I bore myhouse upon my back!\n");
 }
