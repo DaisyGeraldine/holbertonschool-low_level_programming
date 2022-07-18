76d78630d35778d52a0325d164d4fc7e84d8188d
DaisyGeraldine <chipanal@hotmail.com>
2022-07-17 20:39:52 -0500

    frees a listint_t list

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * free_listint2 - task 05
+ * @head : pointer of struct list_t
+ * Description: function that frees a listint_t list.
+ * Return: always
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (!*head)
+		return;
+
+	while (*head)
+	{
+		temp = *head;
+		(*head) = (*head)->next;
+		free(temp);
+	}
+}
