f1e96a45ede547428b3a080d8b690e53e0428971
DaisyGeraldine <chipanal@hotmail.com>
2022-07-18 00:26:05 -0500

    returns the nth node of a listint_t linked list

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -5,6 +5,7 @@
 /**
  * get_nodeint_at_index - task 07
  * @head : pointer of struct list_t
+ * @index : index to choose
  * Description: function that returns the nth node of a listint_t linked list.
  * Return: always
  */
