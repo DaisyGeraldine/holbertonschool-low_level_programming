c03bd05d788f05d56f740682766787e3b4c42d5b
DaisyGeraldine <chipanal@hotmail.com>
2022-08-20 18:28:42 -0500

    function that creates a hash table

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -11,7 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
 
-	ht = malloc(size * sizeof(hash_table_t));
+	ht = malloc(size * sizeof(hash_node_t));
 
 	return (ht);
 }
