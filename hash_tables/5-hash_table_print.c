6c7867dd5397433158072248f94224a2faac1ce8
DaisyGeraldine <chipanal@hotmail.com>
2022-08-21 22:26:56 -0500

    modify function has table print

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -10,22 +10,24 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	unsigned long int i;
+	unsigned long int i = 0 ,n = 0;
 	hash_node_t *p = NULL;
-	char n;
+
+	if (!ht)
+		return;
 
 	printf("{");
 	for (i = 0; ht && i < ht->size; i++)
 	{
 		p = ht->array[i];
-		n = 0;
 		while (p)
 		{
-			if (n)
+			n += i;
+			if (n != i)
 				printf(", ");
 			printf("'%s': '%s'", p->key, p->value);
-			p = p-> next;
-			n = 1;
+			p = p->next;
+			n++;
 		}
 	}
 	printf("}\n");
